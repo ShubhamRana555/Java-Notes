058e38361e2fffaa01c0aff2363093d26f481402
Shubham <shubhamrana0108@gmail.com>
2023-08-29 06:12:23 +0530

    added Dutch National Flag Algorithm

--- Arrays/sorting/countsort.java
+++ Arrays/sorting/countsort.java
@@ -30,12 +30,45 @@ public class countsort{
 
     }
 
+    // Dutch National Flag Algorithm
+    public static void swap(int nums[],int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void dnf(int nums[]){
+        int start=0;
+        int curr=0;
+        int end=nums.length-1;
+
+        while(curr <= end){
+            if(nums[curr] == 0){
+                swap(nums,curr,start);
+                curr++;
+                start++;
+            }
+            else if(nums[curr] == 2){
+                swap(nums,curr,end);
+                end--;
+            }
+            else{
+                curr++;
+            }
+        }
+
+    }
 
     public static void main(String[] args) {
         int arr[] = {5,6,3,4,9,1,3,3,3,4,7};
-        countSort(arr);
-        for(int  i:arr){
-            System.out.println(i);
+        // countSort(arr);
+        // for(int  i:arr){
+        //     System.out.println(i);
+        // }
+        int nums[] = {0,2,2,1,1,2,1,0,0};
+        dnf(nums);
+        for(int i:nums){
+            System.out.print(i+" ");
         }
     }
 }
