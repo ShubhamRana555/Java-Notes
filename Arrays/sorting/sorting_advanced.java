07e605b18f30ee00e059645a82d133748dcb3ccb
Shubham <shubhamrana0108@gmail.com>
2023-08-29 20:58:00 +0530

    added sorting algorithms

--- Arrays/sorting/sorting_advanced.java
+++ Arrays/sorting/sorting_advanced.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class countsort{
+public class sorting_advanced{
 
     public static void countSort(int arr[]){
         int n = arr.length;
@@ -59,6 +59,57 @@ public class countsort{
 
     }
 
+    //Merge Sort
+    public static void merge(int[] arr, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            rightArray[j] = arr[middle + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int middle = left + (right - left) / 2;
+
+            mergeSort(arr, left, middle);
+            mergeSort(arr, middle + 1, right);
+
+            merge(arr, left, middle, right);
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = {5,6,3,4,9,1,3,3,3,4,7};
         // countSort(arr);
@@ -70,5 +121,9 @@ public class countsort{
         for(int i:nums){
             System.out.print(i+" ");
         }
+        mergeSort(arr, 0, arr.length - 1);
     }
 }
+    
+
+ 
